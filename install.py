4fb85b8808e5cf04ed1a0b8f5ead0fdbe5c825bd
xsrf.failed <166954060+xsrf-failed@users.noreply.github.com>
2024-08-22 00:06:36 -0400

    Update install.py

--- install.py
+++ install.py
@@ -6,12 +6,12 @@ if sys.platform=='linux': os.system('clear')
 print("Sigma Proxy Installer")
 print("Created by AK")
 if sys.platform == "win32":
-    os.system("pip install requests requests[socks] colorama psutil")
+    os.system("pip install requests requests[socks] colorama psutil fake-useragent")
     os.system('cls')
     os.system("python sigmaproxy_win.py")
 if sys.platform == "linux":
     os.system("sudo apt install python3-full")
-    os.system("pip3 install requests requests[socks] colorama psutil")
+    os.system("pip3 install requests requests[socks] colorama psutil fake-useragent")
     os.system('clear')
     os.system("python3 sigmaproxy_lin.py")
 else:
