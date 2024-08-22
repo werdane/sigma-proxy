94c6756ec7fd01ea2e19c1449e7906b03fd941cc
xsrf.failed <166954060+xsrf-failed@users.noreply.github.com>
2024-08-21 23:55:13 -0400

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,18 @@
 import os
+import sys
+if sys.platform=='win32': os.system('cls')
+if sys.platform=='linux': os.system('clear')
 
-os.system("pip install requests requests[socks] colorama psutil")
-os.system("python tor.py")
+print("Sigma Proxy Installer")
+print("Created by AK")
+if sys.platform == "win32":
+    os.system("pip install requests requests[socks] colorama psutil")
+    os.system('cls')
+    os.system("python sigmaproxy_win.py")
+if sys.platform == "linux":
+    os.system("sudo apt install python3-full")
+    os.system("pip3 install requests requests[socks] colorama psutil")
+    os.system('clear')
+    os.system("python3 sigmaproxy_lin.py")
+else:
+    print("OS not supported")
